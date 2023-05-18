c77cf49642120b780cdb2afc9ceaa4ece9f1ff70
Noxmore <noxmore@corrupteduser.com>
2023-05-18 15:19:14 -0700

    Made it functional

--- src/animators.rs
+++ src/animators.rs
@@ -1,9 +1,129 @@
 use crate::prelude::*;
 
-pub struct LinearAnimator;
+/// The simplest animation function, goes in a straight line.
+pub fn linear(f: f32) -> f32 { f }
 
-impl Animator for LinearAnimator {
-	fn update(dt: f32, value: f32, target: f32, duration: f32) -> f32 {
-		value.lerp(target, t)
+/// Ease in animator, the lower the weight is, the closer to a straight line the animation will be, a good starting weight is 3.
+pub fn ease_in(weight: f32) -> impl Animator { move |f| f.powf(weight) }
+/// Ease out animator, the lower the weight is, the closer to a straight line the animation will be, a good starting weight is 3.
+pub fn ease_out(weight: f32) -> impl Animator { move |f| 1. - (1. - f).powf(weight) }
+/// Shorthand for using [in_out] with [ease_in], and [ease_out] both using the same weight.
+pub fn ease_in_out(weight: f32) -> impl Animator { in_out(ease_in(weight), ease_out(weight)) }
+
+/// Transforms `f` to a sigmoid value using sin function in the range `0.0..1.0`. Produces an ease-in-ease-out result.
+pub fn sin_sigmoid(f: f32) -> f32 { (( (f - 0.5) * std::f32::consts::PI).sin() + 1. ) / 2. }
+
+
+
+/// Combines 2 animators, `start` occuring on the starting half of the animation, and `end` occuring on the second half of the animation.
+/// 
+/// Useful for ease-in-ease-out animations, but it's recommended to use [sin_sigmoid] or [ease_in_out] if possible for more concise code.
+pub fn in_out(start: impl Animator, end: impl Animator) -> impl Animator {
+	move |f|
+	tr!(f < 0.5 =>
+		start(f * 2.) / 2.
+		; end((f - 0.5) * 2.) / 2. + 0.5
+	)
+}
+
+
+/// Invokes the specified [Animator] in half the time, and for the other half, run the same animator, but flipped, creating a spike-like curve.
+pub fn spike(anim: impl Animator) -> impl Animator {
+	move |f|
+	tr!(f <= 0.5 =>
+		anim(f / 0.5)
+		; anim((1. - f) / 0.5)
+	)
+}
+/// Like [spike], but stays at the end position for a certain amount of time before returning.
+/// 
+/// `time`: A range from `0..1` representing the percentage of the time should be idle (e.g. 0.5 = half the time), increasing this also makes the transition durations faster.
+pub fn spike_stay(time: f32, anim: impl Animator) -> impl Animator {
+	let transition_time = 0.5 - time / 2.;
+	move |f|
+	tr!(f <= transition_time =>
+			anim(f / transition_time)
+		, f >= 1. - transition_time =>
+			anim((1. - f) / transition_time)
+		; 1.
+	)
+}
+
+/*
+/// Lerps between 2 animators.
+pub struct InOut<In: Animator, Out: Animator> {
+	in_anim: In,
+	out_anim: Out,
+}
+impl<In: Animator, Out: Animator> Animator for InOut<In, Out> {
+	fn get(&mut self, fraction: f32) -> f32 {
+		self.in_anim.get(fraction).lerp(self.out_anim.get(fraction), fraction)
 	}
 }
+impl<In: Animator, Out: Animator> InOut<In, Out> {
+	/// Creates a new [Both] with the specified animators.
+	pub fn new(in_anim: In, out_anim: Out) -> Self { Self { in_anim, out_anim } }
+}
+
+/// TODO: Document
+pub struct Spike<Anim: Animator> {
+	anim: Anim,
+}
+impl<Anim: Animator> Animator for Spike<Anim> {
+	fn get(&mut self, fraction: f32) -> f32
+	{
+		tr!(fraction <= 0.5 =>
+			self.anim.get(fraction / 0.5)
+			; self.anim.get((1. - fraction) / 0.5)
+		)
+	}
+}
+impl<Anim: Animator> Spike<Anim> {
+	/// Creates a new [Spike] with the specified [Animator]
+	pub fn new(anim: Anim) -> Self { Self { anim } }
+
+	pub fn stay(anim: Anim, time: f32) -> Self { todo!() }
+}
+
+/// The simplest animator, goes in a streight line.
+pub struct Linear;
+impl Animator for Linear {
+	fn get(&mut self, fraction: f32) -> f32 {
+		fraction
+	}
+}
+
+/// Smoothly eases in and out TODO: Document
+pub struct Ease {
+	weight: i32,
+	flip: bool,
+}
+impl Animator for Ease {
+	fn get(&mut self, fraction: f32) -> f32
+	{
+		tr!(self.flip =>
+			1. - (1. - fraction).powi(self.weight)
+			; fraction.powi(self.weight)
+		)
+	}
+}
+impl Ease {
+	/// Ease in function, `in` is a rust keyword unfortunately, so it has to be `in_`
+	pub fn in_(weight: i32) -> Self {
+		Self {
+			weight,
+			flip: false,
+		}
+	}
+
+	/// Ease out function
+	pub fn out(weight: i32) -> Self {
+		Self { weight, flip: true }
+	}
+
+	/// Create a [InOut] with a [Ease] in and [Ease] out, with with the same weight
+	pub fn in_out(weight: i32) -> InOut<Self, Self> {
+		InOut::new(Self::in_(weight), Self::out(weight))
+	}
+}
+*/
--- src/lib.rs
+++ src/lib.rs
@@ -4,42 +4,106 @@ pub use glam;
 pub mod prelude;
 pub mod animators;
 
-use std::{collections::HashMap, time::Instant};
+use std::{collections::{HashMap, hash_map::DefaultHasher}, time::Instant, ops, hash::{Hash, Hasher}};
+use keystone::*;
 use prelude::*;
 
 
-#[derive(Debug)]
-pub struct Animus<'a>
+#[derive(Clone, Debug, Default)]
+pub struct Animus
 {
-	pub last_update: Instant,
-	pub animations: HashMap<Uuid, Animation<'a>>,
+	pub animations: HashMap<u64, (bool, Animation)>,
 }
 
-impl<'a> Animus<'a>
+impl Animus
 {
-	pub fn update(&mut self)
-	{
+	/*fn new_anim(&mut self, id: u64, start: f32, end: f32, duration: f32) -> Animation {
+		let anim = Animation::new(start, end, duration);
+		self.animations.insert(id, (true, anim));
+		anim
+	}*/
 
+	pub fn anim(&mut self, id: impl Hash, start: f32, end: f32, duration: f32, animator: impl Animator) -> f32
+	{
+		let mut hasher = DefaultHasher::new();
+		id.hash(&mut hasher);
+		let id = hasher.finish();
+
+		/*let anim =
+			if let Some((_animating, anim)) = self.animations.get_mut(&id) {
+				if anim.start != start || anim.end != end || anim.duration != duration {
+					&mut self.new_anim(id, start, end, duration)
+				}
+				else { anim }
+			}
+			else { &mut self.new_anim(id, start, end, duration) };*/
+
+		if !self.animations.contains_key(&id) {
+			self.animations.insert(id, (true, Animation::new(start, end, duration)));
+		}
+
+		let (animating, anim) = self.animations.get_mut(&id).unwrap();
+
+		let fraction = anim.start_time.elapsed().as_secs_f32() / anim.duration;
+		if fraction <= 0. || fraction >= 1. { *animating = false }
+		anim.start.lerp_bounded(anim.end, animator(fraction))
 	}
 
-	pub fn animate_value(&mut self, id: impl Into<Uuid>, value: &mut f32, target: f32, duration: f32, animator: impl Animator) -> f32
-	{
+	pub fn is_animating(&self) -> bool {
+		!self.animations.is_empty()
+		// TODO: Check is animations are done
+	}
 
+	pub fn clear_animations(&mut self) {
+		self.animations.clear();
+	}
+
+
+	/// Call at the end of every update/frame, removes animations that are not in use.
+	pub fn gc(&mut self)
+	{
+		// println!("before: {:#?}", self.animations);
+		self.animations = self.animations.clone().into_iter().filter(|(_id, (animating, _anim))| *animating).collect();
+		// println!("after: {:#?}", self.animations);
 	}
 }
 
-#[derive(Debug)]
-pub struct Animation<'a>
+#[derive(Clone, Copy, Debug)]
+pub struct Animation
 {
-	value: &'a mut f32,
-	duration: f32,
-	target: f32,
+	pub start: f32,
+	pub end: f32,
+
+	pub start_time: Instant,
+	pub duration: f32,
 	// animator: Box<dyn Animator>,
 }
 
-
-// pub trait Animatable: ;
-
-pub trait Animator {
-	fn update(dt: f32, value: f32, target: f32, duration: f32) -> f32;
+impl Animation
+{
+	pub fn new(start: f32, end: f32, duration: f32) -> Self {
+		Self {start, end, start_time: Instant::now(), duration}
+	}
 }
+
+
+/// Currently not used
+pub trait Animatable: Sized + Lerp<f32> + ops::Mul + ops::MulAssign + ops::Add + ops::AddAssign {}
+impl<T: Sized + Lerp<f32> + ops::Mul + ops::MulAssign + ops::Add + ops::AddAssign> Animatable for T {}
+
+
+
+
+/*pub trait Animator
+{
+	fn get_from_animation(&mut self, anim: Animation, fraction: f32) -> f32 {
+		anim.start.lerp_bounded(anim.end, self.get(fraction))
+	}
+
+	fn get(&mut self, fraction: f32) -> f32;
+}*/
+
+pub trait Animator: FnOnce(f32) -> f32 {}
+impl<T: FnOnce(f32) -> f32> Animator for T {}
+
+// pub type Animator = FnOnce(f32) -> f32;
